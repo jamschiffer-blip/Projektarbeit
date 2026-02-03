9b6b49aed83bf7525b82481dc91bb0c992c493e6
jamschiffer-blip <jan_schiffer@gmx.net>
2026-02-03 15:02:03 +0100

    Überabeitung Symbolleiste und Erstellung der Menübar

--- src/main/java/org/example/GUI.java
+++ src/main/java/org/example/GUI.java
@@ -5,44 +5,69 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JTextArea;
-import javax.swing.JToolBar;
-import javax.swing.KeyStroke;
+import javax.swing.*;
 
 public class GUI extends JFrame {
+
+    private JButton Undo,Redo,Speichern;
+    private JPanel leiste;
+    private JToolBar symbolleiste;
+    private JMenuBar menueleiste;
+    private JMenu datei;
+    private JMenuItem Speichernmenueleiste , Laden, NeueDatei ;
 
 
     public GUI(){
         super("Graphic Editor");
         setSize(500,700);
 
+        MeinListener listener = new MeinListener();
 
         setLayout(new BorderLayout());
 
-        JToolBar symbolleiste = new JToolBar();
-        symbolleiste.setFloatable(false);
+        symbolleiste = new JToolBar();
+        symbolleiste.setFloatable(false); //Kann man mit der Maus nicht mehr wegziehen
 
-        JButton Undo = new JButton("Undo");
-        //hier image statt text
-        JButton Redo = new JButton("Redo");
-        JButton Speichern = new JButton("Speichern");
+        Undo = new JButton();
+        Undo.setIcon(new ImageIcon("icons/undo.png"));
+        Redo = new JButton();
+        Redo.setIcon(new ImageIcon("icons/redo.png"));
+        Speichern = new JButton();
+        Speichern.setIcon(new ImageIcon("icons/save.png"));
 
         Undo.setActionCommand("undo");
-        Redo.setActionCommand("redo");
+        Redo.setActionCommand("redo");  //Icons Plagiat!!!
         Speichern.setActionCommand("speichern");
 
         symbolleiste.add(Undo);
         symbolleiste.add(Redo);
         symbolleiste.add(Speichern);
+        symbolleiste.setLayout(new FlowLayout(FlowLayout.LEFT));
 
-        add(symbolleiste);
+        leiste = new JPanel();
+        leiste.setLayout(new BoxLayout(leiste,BoxLayout.Y_AXIS));
+        leiste.add(symbolleiste,BorderLayout.WEST); //hiermit bleibt die leiste immer links in der ecke,auch wenn das fenster vergroessert oder verkleinert wird
+
+        menueleiste = new JMenuBar();
+        datei = new JMenu("Datei");
+        Speichernmenueleiste = new JMenuItem("Speichern");
+        Laden = new JMenuItem("Laden");
+        NeueDatei = new JMenuItem("Neue Datei");
+        Laden.setActionCommand("laden");
+        Speichernmenueleiste.setActionCommand("speichern");
+        NeueDatei.setActionCommand("neue datei");
+
+        datei.add(Speichernmenueleiste);
+        datei.add(Laden);
+        datei.add(NeueDatei);
+
+        menueleiste.setLayout(new FlowLayout(FlowLayout.LEFT));
+        menueleiste.add(datei);
+
+        leiste.add(menueleiste);
+
+
+        add(leiste,BorderLayout.NORTH);
 
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
