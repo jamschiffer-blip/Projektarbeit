872e5583a38a04a2bcb739fcc7bacb67dfe7e99c
jamschiffer-blip <jan_schiffer@gmx.net>
2026-02-04 12:58:08 +0100

    Erstellen und erster Entwurf des Canvas als Hilfsklasse

--- src/main/java/org/example/Zeichenflaeche.java
+++ src/main/java/org/example/Zeichenflaeche.java
@@ -1,4 +1,28 @@
 package org.example;
 
-public class Zeichenflaeche {
+import javax.sound.sampled.Line;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zeichenflaeche extends JPanel {
+    private List<Linie> Linien;
+
+    public Zeichenflaeche() {
+        Linien = new ArrayList<>(); //hier werden die Linien die gezeichnet werden erstgespeichert
+    }
+    public void zeichneLinie(Linie linie){
+        Linien.add(linie);
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) { //überschreiben von paintcomponent anstatt graphics2d wäre sonst zzu gross
+        super.paintComponent(g);
+        Graphics2D grafik = (Graphics2D) g ;
+
+        for(Linie linie : Linien)
+            grafik.drawLine(linie.getStartX(),linie.getStartY(),linie.getEndX(), linie.getEndY());
+    }
 }
